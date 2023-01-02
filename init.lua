f3a863454ae4cf3036dfb84cbb0525f643df1e1d
Sokhuong Uon <sokhuong.usk@gmail.com>
2023-01-02 20:12:32 +0700

    feat: add folding

--- init.lua
+++ init.lua
@@ -25,13 +25,24 @@ local file = {
 
   showInExplorer = function()
     vim.fn.VSCodeNotify("workbench.files.action.showActiveFileInExplorer")
+  end,
+
+  rename = function()
+    vim.fn.VSCodeNotify("workbench.files.action.showActiveFileInExplorer")
+    vim.fn.VSCodeNotify("renameFile")
   end
 }
 
-local problem = {
-  toggle = function()
-    vim.fn.VSCodeNotify("workbench.actions.view.toggleProblems")
-  end
+local error = {
+  list = function()
+    vim.fn.VSCodeNotify("workbench.actions.view.problems")
+  end,
+  next = function()
+    vim.fn.VSCodeNotify("editor.action.marker.next")
+  end,
+  previous = function()
+    vim.fn.VSCodeNotify("editor.action.marker.prev")
+  end,
 }
 
 local editor = {
@@ -65,6 +76,9 @@ local toggle = {
   end,
   toggleZenMode = function()
     vim.fn.VSCodeNotify("workbench.action.toggleZenMode")
+  end,
+  theme = function()
+    vim.fn.VSCodeNotify("workbench.action.selectTheme")
   end,
 }
 
@@ -106,14 +120,26 @@ local git = {
   init = function()
     vim.fn.VSCodeNotify("git.init")
   end,
+  status = function()
+    vim.fn.VSCodeNotify("workbench.view.scm")
+  end,
   switch = function()
     vim.fn.VSCodeNotify("git.checkout")
+  end,
+  deleteBranch = function()
+    vim.fn.VSCodeNotify("git.deleteBranch")
   end,
   push = function()
     vim.fn.VSCodeNotify("git.push")
   end,
   pull = function()
     vim.fn.VSCodeNotify("git.pull")
+  end,
+  fetch = function()
+    vim.fn.VSCodeNotify("git.fetch")
+  end,
+  commit = function()
+    vim.fn.VSCodeNotify("git.commit")
   end,
   publish = function()
     vim.fn.VSCodeNotify("git.publish")
@@ -123,6 +149,41 @@ local git = {
   graph = function()
     vim.fn.VSCodeNotify("gitlens.showGraphPage")
   end,
+}
+
+local fold = {
+  toggle = function()
+    vim.fn.VSCodeNotify("editor.toggleFold")
+  end,
+
+  all = function()
+    vim.fn.VSCodeNotify("editor.foldAll")
+  end,
+  openAll = function()
+    vim.fn.VSCodeNotify("editor.unfoldAll")
+  end,
+
+  close = function()
+    vim.fn.VSCodeNotify("editor.fold")
+  end,
+  open = function()
+    vim.fn.VSCodeNotify("editor.unfold")
+  end,
+  openRecursive = function()
+    vim.fn.VSCodeNotify("editor.unfoldRecursively")
+  end,
+
+  blockComment = function()
+    vim.fn.VSCodeNotify("editor.foldAllBlockComments")
+  end,
+
+  allMarkerRegion = function()
+    vim.fn.VSCodeNotify("editor.foldAllMarkerRegions")
+  end,
+  openAllMarkerRegion = function()
+    vim.fn.VSCodeNotify("editor.unfoldAllMarkerRegions")
+  end,
+
 }
 
 -- https://vi.stackexchange.com/a/31887
@@ -145,11 +206,16 @@ vim.keymap.set({ 'n', 'v' }, "<leader> ", workbench.showCommands)
 vim.keymap.set({ 'n', 'v' }, "H", workbench.previousEditor)
 vim.keymap.set({ 'n', 'v' }, "L", workbench.nextEditor)
 
-vim.keymap.set({ 'n' }, "<leader>gs", git.switch)
+vim.keymap.set({ 'n' }, "<leader>el", error.list)
+vim.keymap.set({ 'n' }, "<leader>en", error.next)
+vim.keymap.set({ 'n' }, "<leader>ep", error.previous)
+
+vim.keymap.set({ 'n' }, "<leader>gb", git.switch)
 vim.keymap.set({ 'n' }, "<leader>gi", git.init)
-vim.keymap.set({ 'n' }, "<leader>gp", git.push)
-vim.keymap.set({ 'n' }, "<leader>gP", git.pull)
-vim.keymap.set({ 'n' }, "<leader>gU", git.publish)
+vim.keymap.set({ 'n' }, "<leader>gd", git.deleteBranch)
+vim.keymap.set({ 'n' }, "<leader>gf", git.fetch)
+vim.keymap.set({ 'n' }, "<leader>gs", git.status)
+vim.keymap.set({ 'n' }, "<leader>gp", git.pull)
 vim.keymap.set({ 'n' }, "<leader>gg", git.graph)
 
 vim.keymap.set({ 'n' }, "<leader>pf", project.findFile)
@@ -158,17 +224,19 @@ vim.keymap.set({ 'n' }, "<leader>pt", project.tree)
 
 vim.keymap.set({ 'n', 'v' }, "<space>w", file.save)
 vim.keymap.set({ 'n', 'v' }, "<space>wa", file.saveAll)
+vim.keymap.set({ 'n', 'v' }, "<space>fs", file.save)
+vim.keymap.set({ 'n', 'v' }, "<space>fS", file.saveAll)
 vim.keymap.set({ 'n' }, "<space>ff", file.format)
 vim.keymap.set({ 'n' }, "<space>ft", file.showInExplorer)
+vim.keymap.set({ 'n' }, "<space>fr", file.rename)
 
 vim.keymap.set({ 'n', 'v' }, "<space>c", editor.closeActive)
 vim.keymap.set({ 'n', 'v' }, "<space>k", editor.closeOther)
 
-vim.keymap.set({ 'n', 'v' }, "<space>e", problem.toggle)
-
 vim.keymap.set({ 'n', 'v' }, "<leader>ta", toggle.toggleActivityBar)
 vim.keymap.set({ 'n', 'v' }, "<leader>tz", toggle.toggleZenMode)
 vim.keymap.set({ 'n', 'v' }, "<leader>ts", toggle.toggleSideBarVisibility)
+vim.keymap.set({ 'n', 'v' }, "<leader>tt", toggle.theme)
 
 vim.keymap.set({ 'n' }, "<leader>rr", symbol.rename)
 
@@ -176,4 +244,14 @@ vim.keymap.set({ 'n' }, "<leader>sr", search.reference)
 vim.keymap.set({ 'n' }, "<leader>sR", search.referenceInSideBar)
 vim.keymap.set({ 'n' }, "<leader>sp", search.project)
 vim.keymap.set({ 'n' }, "<leader>st", search.text)
+
+vim.keymap.set({ 'n' }, "<leader>zr", fold.openAll)
+vim.keymap.set({ 'n' }, "<leader>zO", fold.openRecursive)
+vim.keymap.set({ 'n' }, "<leader>zo", fold.open)
+vim.keymap.set({ 'n' }, "<leader>zm", fold.all)
+vim.keymap.set({ 'n' }, "<leader>zb", fold.blockComment)
+vim.keymap.set({ 'n' }, "<leader>zc", fold.close)
+vim.keymap.set({ 'n' }, "<leader>zg", fold.allMarkerRegion)
+vim.keymap.set({ 'n' }, "<leader>zG", fold.openAllMarkerRegion)
+vim.keymap.set({ 'n' }, "<leader>za", fold.toggle)
 --#endregion keymap
